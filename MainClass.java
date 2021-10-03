5f7818c50e4bb40205c7f7cacd272cf7376c7881
sparksu005 <91057614+sparksu005@users.noreply.github.com>
2021-10-03 13:16:56 +0800

    Add files via upload

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+package s1;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		Station s1 = new Station("window 1");
+		Station s2 = new Station("window 2");
+		Station s3 = new Station("window 3");
+
+	}
+
+}
